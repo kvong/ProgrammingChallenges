571a140cfe6f53f17992cc2c8aef9ae1a3b1ec48
kvong <>
2022-03-14 22:53:44 -0500

    comments added

--- AlgorithmicThinking/1.HashTables/1.UniqueSnowflakes/main.c
+++ AlgorithmicThinking/1.HashTables/1.UniqueSnowflakes/main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/******************************************************************************
+* File:             main.c
+*
+* Author:           Khanh Vong  
+* Created:          03/14/22 
+* Description:      Practice implementing a hashtable to quickly find similar 
+*                   snowflakes
+*****************************************************************************/
+
+
+// Set the size of the hash table
 #define HASHMOD 1000
 
 /* Type declarations */
-
 typedef struct Snowflake {
-    int legs[6];
+    int arms[6];
     int groupId;
 } Snowflake;
 
@@ -25,6 +35,7 @@ int main(int argc, char *argv[])
     int i;
     HashNode* hashTable[HASHMOD];
 
+    // Set everything in the hashtable to NULL
     for ( i = 0; i < HASHMOD; i++ ) {
         hashTable[i] = NULL;
     }
@@ -35,10 +46,16 @@ int main(int argc, char *argv[])
     Snowflake snowflakes[100000];
     int hash;
     for ( i = 0; i < numOfSnowflakes; i++ ) {
-        scanf( "%d %d %d %d %d %d", &snowflakes[i].legs[0], &snowflakes[i].legs[1], &snowflakes[i].legs[2], &snowflakes[i].legs[3], &snowflakes[i].legs[4], &snowflakes[i].legs[5] );
-        hash = snowflakes[i].legs[0] + snowflakes[i].legs[1] + snowflakes[i].legs[2] + snowflakes[i].legs[3] + snowflakes[i].legs[4] + snowflakes[i].legs[5];
+        // Read  snowflakes from stdin
+        scanf( "%d %d %d %d %d %d", &snowflakes[i].arms[0], &snowflakes[i].arms[1], &snowflakes[i].arms[2], &snowflakes[i].arms[3], &snowflakes[i].arms[4], &snowflakes[i].arms[5] );
+
+        // Make the hash the sum of the snowflake's arms
+        hash = snowflakes[i].arms[0] + snowflakes[i].arms[1] + snowflakes[i].arms[2] + snowflakes[i].arms[3] + snowflakes[i].arms[4] + snowflakes[i].arms[5];
         
+        // Bound the hash with HASHMOD defined on line 1
         hash = hash % HASHMOD;
+
+        // Add the snowflake as a linked list to the hash table index
         HashNode* hashNode = (HashNode*)malloc(sizeof(HashNode));
         hashNode->snowflake = snowflakes[i];
         hashNode->nextSnowflake = hashTable[hash];
@@ -46,14 +63,21 @@ int main(int argc, char *argv[])
     }
 
     int leftMatches, rightMatches;
+
+    // Go through every entry of the hash table
     for (i = 0; i < HASHMOD; i++) {
         if (hashTable[i] != NULL){
             HashNode* currentNode = hashTable[i];
+            
+            // Traverse the linked list only if there's a comparison in the list
             while (currentNode != NULL && currentNode->nextSnowflake != NULL){
                 HashNode* nextNode = currentNode->nextSnowflake;
+
+                // Traverse next node
                 while (nextNode != NULL){
-                    leftMatches = compareLeft( currentNode->snowflake.legs, nextNode->snowflake.legs);
-                    rightMatches = compareRight( currentNode->snowflake.legs, nextNode->snowflake.legs);
+                    leftMatches = compareLeft( currentNode->snowflake.arms, nextNode->snowflake.arms);
+                    rightMatches = compareRight( currentNode->snowflake.arms, nextNode->snowflake.arms);
+                    // Check if there was a match during the left or right comparison
                     if ( leftMatches == 6 || rightMatches == 6 ) {
                         printf("Twin snowflakes found.\n");
                         cleanHashTable(hashTable);
@@ -72,6 +96,7 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+/* Deallocate all allocated memory from hashtable */
 int cleanHashTable(HashNode* hashTable[HASHMOD]) {
     int i;
     for (i = 0; i < HASHMOD; i++) {
@@ -87,53 +112,63 @@ int cleanHashTable(HashNode* hashTable[HASHMOD]) {
 }
 
 
+/* Compare the arms of a snowflake form left to right */
 int compareRight(int currentFlake[], int nextFlake[]) {
     int i;
-    int currentLeg = 0;
-    int matchLegs = 0;
-    for ( i = 0; i < 6; i++ ) {
-        if ( currentFlake[currentLeg] == nextFlake[i]) {
-            matchLegs = 0;
-            int k;
-            for (k = 0; k < 6; k++ ) {
-                int index = i + k;
-                if ( index > 5 ) {
-                    index -= 6;
-                }
+    int currentArm = 0;
+    int matchArms = 0;
 
-                if ( currentFlake[currentLeg + k % 6] == nextFlake[index] ) {
-                    if (matchLegs == 6) {
-                        return matchLegs;
+    // Go through all sides of the nextFlake
+    for ( i = 0; i < 6; i++ ) {
+
+        // Find snowflake arm that match with the currentFlake curent arm
+        // current flake's current arm must be in the nextFlake
+        if ( currentFlake[currentArm] == nextFlake[i]) {
+            matchArms = 0;
+            int k;
+
+            // Now that we have found a common arm,
+            // from current arm position going to the right,
+            // check if the next arm is similar to currentFlake's next arm.
+            for (k = 0; k < 6; k++ ) {
+                int index = (i + k) % 6; // Use modulus to cycle right: after 6 is 0
+
+                if ( currentFlake[currentArm + k % 6] == nextFlake[index] ) {
+                    if (matchArms == 6) {
+                        return matchArms;
                     }
-                    matchLegs++;
+                    matchArms++;
                 }
             }
         }
     }
-    return matchLegs;
+    return matchArms;
 }
 
+/* Compare the arms of a snowflake form right to left */
 int compareLeft(int currentFlake[], int nextFlake[]) {
     int i;
-    int currentLeg = 0;
-    int matchLegs = 0;
+    int currentArm = 0;
+    int matchArms = 0;
+
+    // Go through all sides of the nextFlake and compare witht the currentFlake's current arm
     for ( i = 0; i < 6; i++ ) {
-        if ( currentFlake[currentLeg] == nextFlake[i]) {
-            matchLegs = 0;
+        
+        // After finding the matching arm start comparing the remaining arms
+        if ( currentFlake[currentArm] == nextFlake[i]) {
+            matchArms = 0;
             int k;
+
             for (k = 0; k < 6; k++ ) {
-                int index = i - k;
-                if ( index < 0 ) {
-                    index += 6;
-                }
-                if ( currentFlake[currentLeg + k % 6] == nextFlake[index] ) {
-                    if (matchLegs == 6) {
-                        return matchLegs;
+                int index = (i - k + 6) % 6; // Use modulus to cycle left: after 0 is 6
+                if ( currentFlake[currentArm + k % 6] == nextFlake[index] ) {
+                    if (matchArms == 6) {
+                        return matchArms;
                     }
-                    matchLegs++;
+                    matchArms++;
                 }
             }
         }
     }
-    return matchLegs;
+    return matchArms;
 }
