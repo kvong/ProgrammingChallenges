58a9fb1cd5c03cbbecd6061cc758597b44d557af
blank <22939492+kvong@users.noreply.github.com>
2022-06-27 23:15:16 -0500

    need to account for perfect fit case

---
+++ AlgorithmicThinking/3.DynamicProgramming/1.BurgerFervor/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int solve( int burger_a, int burger_b, int time, int counter );
+int max( int x, int y );
+int * memo;
+int main(int argc, char *argv[])
+{
+    int i;
+    int a, b, t;
+    scanf("%d %d %d", &a, &b, &t);
+    memo = malloc( sizeof(int *) * t );
+    for (i = 0; i < t; i++) {
+        memo[i] = NULL;
+    }
+    solve( a, b, t, -1);
+    for ( i = 0; i < t; i++ ){
+        if ( memo[i] != NULL ) {
+            printf( "%d %d\n", i, memo[i] );
+        }
+    }
+
+    return 0;
+}
+
+int max( int x, int y ) {
+    if ( x > y )
+        return x;
+    return y;
+}
+
+int solve( int burger_a, int burger_b, int time, int counter ) {
+    if ( time == 0 )
+        return counter;
+    else if ( time < 0 )
+        return NULL;
+    else
+        counter++;
+    int after_eating_a = 0, after_eating_b = 0;
+    if ( time >= burger_a ) {
+        // Eating burger_a
+        after_eating_a = solve( burger_a, burger_b, time - burger_a, counter );
+    }
+
+    if ( time >= burger_b ) {
+        // Eating burger_b
+        after_eating_b = solve( burger_a, burger_b, time - burger_b, counter );
+    }
+
+
+    if ( memo[time] == NULL || memo[time] < counter )
+        memo[time] = counter;
+
+    return memo[time];
+}
+
