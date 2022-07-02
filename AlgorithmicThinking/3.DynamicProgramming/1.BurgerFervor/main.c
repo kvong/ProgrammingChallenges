67700f0e57a106a57f35e8e21896a31bd6022515
blank <22939492+kvong@users.noreply.github.com>
2022-07-01 23:33:44 -0500

    comments added for recursive solution

--- AlgorithmicThinking/3.DynamicProgramming/1.BurgerFervor/main.c
+++ AlgorithmicThinking/3.DynamicProgramming/1.BurgerFervor/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int solve( int burger_a, int burger_b, int time, int counter );
+int solve_r( int burger_a, int burger_b, int time, int counter );
 int max( int x, int y );
 int * memo;
 int main(int argc, char *argv[])
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     for (i = 0; i < t; i++) {
         memo[i] = -2;
     }
-    solve( a, b, t, -1);
+    solve_r( a, b, t, -1);
     for ( i = 0; i < t; i++ ){
         if ( memo[i] != -2 ) {
             printf( "%d min drinking bear. ate %d burgers\n", i, memo[i] );
@@ -30,29 +30,37 @@ int max( int x, int y ) {
     return y;
 }
 
-int solve( int burger_a, int burger_b, int time, int counter ) {
-
+int solve_r( int burger_a, int burger_b, int time, int counter ) {
+    // Case where it's a perfect fit
     if ( time == 0 ) {
-        counter++;
-        memo[time] = max( memo[time], counter );
+        counter++; // Eating the last burger
+
+        // Check if we're able to eat more burgers
+        // with this strategy than our previous one
+        memo[time] = max( memo[time], counter ); 
         return counter;
     }
-    counter++;
+
+    counter++; // Eating the burger
 
     int after_eating_a, after_eating_b;
     after_eating_a = after_eating_b = counter;
 
-    if ( time >= burger_a ) {
+    if ( time >= burger_a ) { // Can we eat burger_a? 
         // Eating burger_a
-        after_eating_a = solve( burger_a, burger_b, time - burger_a, counter );
+        after_eating_a = solve_r( burger_a, burger_b, time - burger_a, counter );
     }
 
-    if ( time >= burger_b ) {
+    if ( time >= burger_b ) { // Can we eat burger_b?
         // Eating burger_b
-        after_eating_b = solve( burger_a, burger_b, time - burger_b, counter );
+        after_eating_b = solve_r( burger_a, burger_b, time - burger_b, counter );
     }
 
+    // Get the burger that fits the better strategy
     int max_tmp = max( after_eating_a, after_eating_b );
+
+    // Save result if the results are better than
+    // what we currently have (more burgers than before?).
     memo[time] = max( memo[time], max_tmp );
 
     return memo[time];
