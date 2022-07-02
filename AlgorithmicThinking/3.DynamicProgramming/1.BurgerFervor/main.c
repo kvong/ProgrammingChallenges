2b05789aae67dc582137db96d087d89bc5ce1410
blank <22939492+kvong@users.noreply.github.com>
2022-07-01 22:55:52 -0500

    implemented recursive solution

--- AlgorithmicThinking/3.DynamicProgramming/1.BurgerFervor/main.c
+++ AlgorithmicThinking/3.DynamicProgramming/1.BurgerFervor/main.c
@@ -11,12 +11,13 @@ int main(int argc, char *argv[])
     scanf("%d %d %d", &a, &b, &t);
     memo = malloc( sizeof(int *) * t );
     for (i = 0; i < t; i++) {
-        memo[i] = NULL;
+        memo[i] = -2;
     }
     solve( a, b, t, -1);
     for ( i = 0; i < t; i++ ){
-        if ( memo[i] != NULL ) {
-            printf( "%d %d\n", i, memo[i] );
+        if ( memo[i] != -2 ) {
+            printf( "%d min drinking bear. ate %d burgers\n", i, memo[i] );
+            break;
         }
     }
 
@@ -30,13 +31,17 @@ int max( int x, int y ) {
 }
 
 int solve( int burger_a, int burger_b, int time, int counter ) {
-    if ( time == 0 )
-        return counter;
-    else if ( time < 0 )
-        return NULL;
-    else
+
+    if ( time == 0 ) {
         counter++;
-    int after_eating_a = 0, after_eating_b = 0;
+        memo[time] = max( memo[time], counter );
+        return counter;
+    }
+    counter++;
+
+    int after_eating_a, after_eating_b;
+    after_eating_a = after_eating_b = counter;
+
     if ( time >= burger_a ) {
         // Eating burger_a
         after_eating_a = solve( burger_a, burger_b, time - burger_a, counter );
@@ -47,9 +52,8 @@ int solve( int burger_a, int burger_b, int time, int counter ) {
         after_eating_b = solve( burger_a, burger_b, time - burger_b, counter );
     }
 
-
-    if ( memo[time] == NULL || memo[time] < counter )
-        memo[time] = counter;
+    int max_tmp = max( after_eating_a, after_eating_b );
+    memo[time] = max( memo[time], max_tmp );
 
     return memo[time];
 }
